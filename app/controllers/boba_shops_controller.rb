c042fff1f59c17723d6405e7f14a0d7f9824fd02
Khoi Nguyen <khoinguyen311@gmail.com>
2021-08-24 13:19:16 -0700

    Add method create

--- app/controllers/boba_shops_controller.rb
+++ app/controllers/boba_shops_controller.rb
@@ -6,4 +6,16 @@ class BobaShopsController < ApplicationController
   def new
     
   end
+
+  def create
+    boba_shop = BobaShop.new({
+      created_at: params[:boba_shop][:created_at],
+      updated_at: params[:boba_shop][:updated_at],
+      name: params[:boba_shop][:name],
+      delivery: params[:boba_shop][:drive_thru],
+      capacity: params[:boba_shop][:capacity]
+    })
+    boba_shop.save
+    redirect_to '/boba_shops'
+  end
 end
