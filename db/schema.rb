508b61ef23e3643806c6d889ed5e4cc2433f2aed
Khoi Nguyen <khoinguyen311@gmail.com>
2021-08-24 14:40:26 -0700

    Update to drive_thru

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_24_201532) do
+ActiveRecord::Schema.define(version: 2021_08_24_213859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2021_08_24_201532) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string "name"
-    t.boolean "delivery"
+    t.boolean "drive_thru"
     t.integer "capacity"
   end
 
