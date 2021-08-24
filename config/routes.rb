859e965c0a39da4ce9a3a5990d2cfbe79d711a6c
Khoi Nguyen <khoinguyen311@gmail.com>
2021-08-24 13:00:11 -0700

    Add post boba_shops#create

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   get '/restaurants/new', to: 'restaurants#new'
   get '/boba_shops/new', to: 'boba_shops#new'
   post '/restaurants', to: 'restaurants#create'
+  post '/boba_shops', to: 'boba_shops#create'
 end
