993c2529935a5db3f6bf46774f3ae9c6ee242d92
Khoi Nguyen <khoinguyen311@gmail.com>
2021-08-24 15:32:51 -0700

    Add patch to boba_shops#update

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get '/boba_shops/new', to: 'boba_shops#new'
   get '/boba_shops/:id', to: 'boba_shops#show'
   get '/boba_shops/:id/edit', to: 'boba_shops#edit'
+  patch '/boba_shops/:id', to: 'boba_shops#update'
   post '/restaurants', to: 'restaurants#create'
   post '/boba_shops', to: 'boba_shops#create'
 end
