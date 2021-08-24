2d59b78122a288ddaa32d1560182e0ea9fa22d95
Khoi Nguyen <khoinguyen311@gmail.com>
2021-08-24 15:10:03 -0700

    Add boba_shops#edit to controlelr

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get '/restaurants/new', to: 'restaurants#new'
   get '/boba_shops/new', to: 'boba_shops#new'
   get '/boba_shops/:id', to: 'boba_shops#show'
+  get '/boba_shops/:id', to: 'boba_shops#edit'
   post '/restaurants', to: 'restaurants#create'
   post '/boba_shops', to: 'boba_shops#create'
 end
