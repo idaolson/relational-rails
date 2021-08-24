e1b8bc798aede67375943bd759cd5a2b191cb93c
Khoi Nguyen <khoinguyen311@gmail.com>
2021-08-24 12:05:23 -0700

    Add get boba shops

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get '/', to: 'welcome#index'
   get '/restaurants', to: 'restaurants#index'
+  get '/boba_shops', to: 'boba_shops#index'
   get '/restaurants/new', to: 'restaurants#new'
   post '/restaurants', to: 'restaurants#create'
 end
