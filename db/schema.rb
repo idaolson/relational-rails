27cfbab2ccb8eae9041efc1ceb6f1424f5490b5a
Khoi Nguyen <khoinguyen311@gmail.com>
2021-08-24 23:03:52 -0700

    Update migration to use timestamps

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_24_213859) do
+ActiveRecord::Schema.define(version: 2021_08_25_060254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "boba_shops", force: :cascade do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "name"
     t.boolean "drive_thru"
     t.integer "capacity"
