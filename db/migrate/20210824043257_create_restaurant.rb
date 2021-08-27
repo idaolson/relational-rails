6d4b6a5dcb0d032175b23bcd19efb109950c182c
Ida Olson <idaolson@gmail.com>
2021-08-26 20:15:31 -0600

    Many many migrations

--- db/migrate/20210824043257_create_restaurant.rb
+++ db/migrate/20210824043257_create_restaurant.rb
@@ -1,13 +1,12 @@
 class CreateRestaurant < ActiveRecord::Migration[5.2]
   def change
     create_table :restaurants do |t|
+      t.timestamps
       t.datetime :created_at
       t.datetime :updated_at
       t.string :name
       t.boolean :delivery
       t.integer :michelin_stars
-
-      t.timestamp
     end
   end
 end
