8c503a84bc484e8eeeac90eecb2d665d574b041b
Ida Olson <idaolson@gmail.com>
2021-08-26 20:15:06 -0600

    Add create pasta to pasta controller

--- app/controllers/pastas_controller.rb
+++ app/controllers/pastas_controller.rb
@@ -1,5 +1,22 @@
 class PastasController < ApplicationController
   def index
-    @pastas = ['pasta', 'PASTA', 'PASTA!!!']
+    @pastas = Pasta.all
+  end
+
+  def create
+    pasta = Pasta.new({
+      name: params[:pasta][:name],
+      is_vegan: params[:pasta][:is_vegan],
+      price: params[:pasta][:price],
+      restaurant_id: params[:pasta][:restaurant_id],
+      })
+
+    pasta.save
+
+    redirect_to '/pastas'
+  end
+
+  def show
+    @pasta = Pasta.find(params[:id])
   end
 end
