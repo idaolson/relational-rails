d85e3f6ae27b3c87cd3bdbf4d4c120c93f782e15
Khoi Nguyen <khoinguyen311@gmail.com>
2021-08-27 04:32:27 -0700

    Remove dates

--- spec/models/boba_shops/boba_shops_spec.rb
+++ spec/models/boba_shops/boba_shops_spec.rb
@@ -1,61 +1,51 @@
 require 'rails_helper'
-require 'date'
 
 describe BobaShop do
+  describe 'associations' do
+    it {should have_many :drinks}
+  end
+
   describe 'methods' do
     before(:each) do
-      @time = DateTime.now
       @store_1 = BobaShop.create!(
-        id: 1,
-        created_at: @time.strftime('%d%m%y %H:%M:%S'),
-        updated_at: @time.strftime('%d%m%y %H:%M:%S'),
         name: 'Sharetea',
         drive_thru: false,
         capacity: 25
       )
       @store_2 = BobaShop.create!(
-        id: 2,
-        created_at: (@time + 1.hour).strftime('%d%m%y %H:%M:%S'),
-        updated_at: (@time + 1.hour).strftime('%d%m%y %H:%M:%S'),
         name: 'Cha4Tea',
         drive_thru: false,
         capacity: 25
       )
       @drink_1 = @store_1.drinks.create!(
-        id: 1,
-        created_at: @time.strftime('%d%m%y %H:%M:%S'),
-        updated_at: @time.strftime('%d%m%y %H:%M:%S'),
         name: 'Okinawan Milk Tea',
         in_stock: true,
         price: 6.99
       )
       @drink_2 = @store_1.drinks.create!(
-        id: 2,
-        created_at: @time.strftime('%d%m%y %H:%M:%S'),
-        updated_at: @time.strftime('%d%m%y %H:%M:%S'),
         name: 'Hazelnut Milk Tea',
         in_stock: true,
         price: 6.99
       )
       @drink_3 = @store_2.drinks.create!(
-        id: 3,
-        created_at: (@time + 1.hour).strftime('%d%m%y %H:%M:%S'),
-        updated_at: (@time + 1.hour).strftime('%d%m%y %H:%M:%S'),
         name: 'Honey Milk Tea',
         in_stock: true,
         price: 6.99
       )
       @drink_4 = @store_2.drinks.create!(
-        id: 4,
-        created_at: (@time + 1.hour).strftime('%d%m%y %H:%M:%S'),
-        updated_at: (@time + 1.hour).strftime('%d%m%y %H:%M:%S'),
         name: 'Taro Milk Tea',
         in_stock: true,
         price: 6.99
       )
     end
 
-    it '#drinks_count' do
+    it 'can order shops by creation time' do
+      expected = [@store_2, @store_1]
+
+      expect(BobaShop.order_by_creation_time).to eq(expected)
+    end
+
+    it 'can count the number of drinks' do
       expect(@store_1.drinks_count).to eq(2)
     end
   end
